5a50dd7bf45f116368ddd61ef1cd6d8eb363dd1c
ben <ben>
2022-01-06 13:29:29 -0800

    Moving code to sample project for presentation

---
+++ shared/library2/nodejs/library2.js
@@ -0,0 +1,3 @@
+module.exports = {
+    getText:() => "Hello library2"
+};
---
+++ skills/skill1/task2/task2.js
@@ -0,0 +1,13 @@
+const {getText} = require("/opt/nodejs/library2");
+
+exports.handler = async ({body = "{}", headers = {}}) => {
+    console.log(body);
+    console.log(headers);
+    return {
+        statusCode: "200",
+        body: JSON.stringify({
+            text: getText()
+        })
+    };
+
+}
