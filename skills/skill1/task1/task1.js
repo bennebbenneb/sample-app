fe692d0c232d560a2fd65b1fa39c74d19b293fe8
ben <ben>
2022-01-06 14:06:19 -0800

    conflict test

--- skills/skill1/task1/task1.js
+++ skills/skill1/task1/task1.js
@@ -1,6 +1,9 @@
 const {getText} = require("/opt/nodejs/library1");
+const conflictTest = require("/opt/nodejs/conflictTest");
 
 exports.handler = async ({body = "{}", headers = {}}) => {
+    console.log("conflictTest");
+    console.log(conflictTest);
     console.log(body);
     console.log(headers);
     return {
